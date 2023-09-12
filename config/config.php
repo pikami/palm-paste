5ad55753cf53b4e0e938bbb9d6dd0141a358c576
Pijus Kamandulis <pkpjuklas@gmail.com>
2023-09-13 00:02:07 +0300

    Added docker-compose.yml

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 function GetConnectionToDB(){
 	//========SQL_CONFIG========//
-	$SQL_Host       = "localhost";
-	$SQL_Database   = "palm-paste";
-	$SQL_User       = "paste";
-	$SQL_Password   = "ckQgRJRhib74XMgVpzmn38uj1MrCcNnK7L9bc7zu";
+	$SQL_Host       = getenv('SQL_HOST') ?: "localhost";
+	$SQL_Database   = getenv('SQL_DB') ?: "palm-paste";
+	$SQL_User       = getenv('SQL_USER') ?: "paste";
+	$SQL_Password   = getenv('SQL_PASS') ?: "ckQgRJRhib74XMgVpzmn38uj1MrCcNnK7L9bc7zu";
 	//========CONNECTION========//
 	$conn = new PDO('mysql:host='.$SQL_Host.';dbname='.$SQL_Database.';charset=utf8mb4', $SQL_User, $SQL_Password);
 	return $conn;
 }
 //========CRON_JOBS=========//
-$CRON_ExpireKey = "b1g51bf6g";
+$CRON_ExpireKey = getenv('CRON_EXPIREKEY') ?: "b1g51bf6g";
 ?>
